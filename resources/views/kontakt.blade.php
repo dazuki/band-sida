e3c47ec82c389abc0130f9d8a07fda50eac8964f
DazUki <absolute.dessan@gmail.com>
2023-11-20 18:55:30 +0100

    heroicons

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -5,7 +5,14 @@
     @endsection
     <div class="text-white mx-auto w-full max-w-4xl p-8 pt-4 mt-8">
         <p class="text-center text-2xl pb-8 font-semibold text-orange-300">
-            <span class="bg-red-300 bg-opacity-10 border-b-2 border-orange-300 px-4 p-2 rounded-lg">KONTAKT</span>
+            <span class="bg-red-300 bg-opacity-10 border-b-2 border-orange-300 px-4 p-2 rounded-lg">KONTAKT<svg
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                    class="inline-block -mt-1 ml-2 w-7 h-7">
+                    <path fill-rule="evenodd"
+                        d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
+                        clip-rule="evenodd" />
+                </svg>
+            </span>
         </p>
         <div class="flex justify-center items-center">
             <div
--- resources/views/spelningar.blade.php
+++ resources/views/spelningar.blade.php
@@ -5,7 +5,16 @@
     @endsection
     <div class="text-white mx-auto w-full max-w-4xl p-8 pt-4 mt-8">
         <p class="text-center text-2xl pb-8 font-semibold text-orange-300">
-            <span class="bg-red-300 bg-opacity-10 border-b-2 border-orange-300 px-4 p-2 rounded-lg">SPELNINGAR</span>
+            <span class="bg-red-300 bg-opacity-10 border-b-2 border-orange-300 px-4 p-2 rounded-lg">SPELNINGAR<svg
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                    class="inline-block -mt-1 ml-2 w-7 h-7">
+                    <path
+                        d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+                    <path fill-rule="evenodd"
+                        d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z"
+                        clip-rule="evenodd" />
+                </svg>
+            </span>
         </p>
         <div class="flex justify-center items-center">
             <div
