02a07b2af8d1f208c74b6df75e192dee6c1c3e9f
DazUki <absolute.dessan@gmail.com>
2024-01-02 21:23:09 +0100

    removed border and bg header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<section class="box-bg border-b-2 border-orange-300 text-white mx-auto w-full top-0 z-50 shadow-lg">
+<section class="bg-transparent text-white mx-auto w-full top-0 z-50">
     <nav class="flex items-center justify-center py-4 sm:py-4 mx-auto max-w-4xl antialiased">
         <div class="text-xs sm:text-lg font-bold sm:font-semibold">
             <a href="/"
